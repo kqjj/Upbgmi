1924e68529329b1fc09ea170d9f94408abbb4c27
kqjj <kqjj4369@gmail.com>
2024-10-08 18:44:19 +0530

    Update b.py

--- b.py
+++ b.py
@@ -13,12 +13,15 @@ from keep_alive import keep_alive
 keep_alive()
 
 TOKEN = '7376682624:AAFuZXujuqAtJaMB8jdZRTCHif8RpwbcH3Y'
-
+MONGO_URI = 'mongodb+srv://MasterBhaiyaa:MasterBhaiyaa@masterbhaiyaa.7bym6.mongodb.net/?retryWrites=true&w=majority&appName=MasterBhaiyaa'
+CHANNEL_ID = -1002255025786
 ADMIN_IDS = [1866816118]
-
+client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
+db = client['MasterBhaiyaa']
+users_collection = db.users
 
 bot = telebot.TeleBot(TOKEN)
-
+blocked_ports = [8700, 20000, 443, 17500, 9031, 20002, 20001]
 user_attack_details = {}
 active_attacks = {}
 
